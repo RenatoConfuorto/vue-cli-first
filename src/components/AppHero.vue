77116f14f6d5ccb285bcf72a36469a1fbddcea46
RenatoConfuorto <renato.confuorto@outlook.it>
2022-05-16 18:52:16 +0200

    Bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <h1>Jumbotron</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure assumenda
+      labore placeat, nisi praesentium quae cumque nulla qui et culpa autem
+      perspiciatis, non ex hic deleniti itaque, at eveniet! Officiis.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHero",
+};
+</script>
+
+<style scoped>
+.app {
+  margin: 0 auto;
+  padding: 0.8rem;
+  background-image: url(../assets/jumbo.png);
+  background-repeat: no-repeat;
+  background-position: center top;
+  background-size: cover;
+  width: 75%;
+  height: 50vh;
+  color: darkgray;
+}
+p {
+  text-align: left;
+}
+</style>
